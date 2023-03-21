5876e0002c91cc6d75e1c3ceebf74ca3ea238291
Colin Sprows <colin.sprows@gmail.com>
2023-03-21 12:29:53 -0400

    testing post seeds, comment data next

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,7 +1,57 @@
 const router = require('express').Router();
+const { User, blogPost, Comment } = require('../models');
+const auth = require('../utils/auth');
+
+// router.get('/', async (req, res) => {
+//   res.render('login');
+// });
 
 router.get('/', async (req, res) => {
+  try {
+    const postBoard = await blogPost.findAll({
+      include: [{ model: User }]
+    });
+
+    const posts = postBoard.map((post) =>
+      post.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
+});
+
+router.get('/blogpost/:id', async (req, res) => {
+    try {
+      const blogpost = await blogPost.findByPk(req.params.id, {
+        include: [{
+          model: User,
+          model: Comment,
+          include: [{ model: User, attributes: ['user_name']}]
+        }]
+      });
+
+      const post = blogpost.get({ plain: true });
+
+      res.render('blogpost', { post, logged_in: req.session.logged_in });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,9 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, blogPost, Comment } = require('../models');
 
 const userData = require('./userData.json');
+const blogpostData = require('./blogpostData.json');
+// const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,6 +12,16 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
+  await blogPost.bulkCreate(blogpostData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  // await Comment.bulkCreate(commentData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
 
   process.exit(0);
 };
